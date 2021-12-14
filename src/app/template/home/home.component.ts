fac638a1824f56f92dccf5affa7182162e22dc69
Gustavo Sierra H <licantropusdraconis@gmail.com>
2021-12-14 03:14:21 -0500

    Home component changes

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ModelIdentify } from 'src/app/models/identify.model';
+import { SecurityService } from 'src/app/services/security.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  sessionStarted: boolean = false;//we start the variable as false, as the user is not logged by default
+  subs: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private securityService: SecurityService) { }
 
   ngOnInit(): void {
+    this.subs = this.securityService.GetUserDataInSession().subscribe((data: ModelIdentify) => {
+      this.sessionStarted = data.isLogged;
+    });
   }
 
 }
